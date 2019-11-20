0c397ffc1d62a8b7e139fb6f10d1878acb85df77
Zakhar Pustovgarov <deadspace1999@mail.ru>
2019-11-20 11:31:08 +0400

    Added comments to lib.js

--- lib.js
+++ lib.js
@@ -1,31 +1,36 @@
-function NotNull(val)
+// Фильтр для удаления пустых элементов массива
+function NotNull(val) 
 {
   return val != "";
 }
 
-function compareLength(a, b)
+// Правила сравнения
+function compareLength(a, b) 
 {
-    if (a.length > b.length) 
+    if (a.length > b.length) // если длина а больше длины b, то a ставиться перед b
     {
         return -1;
     }
-    else if (a.length < b.length) 
+    else if (a.length < b.length) // если длина b больше длины a, то b ставиться перед a
     {
         return 1;
     }
-    else return 0; 
+    else return 0; // в ином случае порядок не меняется
 }
 
+// Функция для удаления лишних пробелов
 function strip (str)
 {   
     return str_res = str.split(" ").filter(NotNull).join(" ");
 }
 
+// Функция для изменения порядка слов в обратную сторону
 function reverse(str)
 {
     return str_res = strip(str).split(" ").reverse().join(" ");
 }
 
+// Функция для сортировки слов по убыванию количества символов в слове
 function sort(str)
 {
     return str_res = strip(str).split(" ").sort(compareLength).join(" ");
