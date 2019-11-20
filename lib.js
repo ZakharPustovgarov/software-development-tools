21171d0137197699511fb5c6b731881e7f3015fd
Zakhar Pustovgarov <deadspace1999@mail.ru>
2019-11-20 11:21:55 +0400

    Updated strip function

--- lib.js
+++ lib.js
@@ -1,29 +1,11 @@
-function strip (str)
+function NotNull(val)
 {
-    var str_new = new String;
-    var check = false;
-    for (var i = 0; i < str.length; i++)
-    {
-        if (str[i] != " " && i == 0)
-        {
-          str_new = str_new + str[i]
-          check = true;
-        }
-        else if (str[i] == " " && str[i+1] == " ")
-        {
+  return val != "";
+}
 
-        }
-        else if (str[i] == " " && i == str.length-1 )
-        {
-
-        }
-        else if (check == false && str[i] == " " && str[i+1] != " ")
-        {
-          check = true;
-        }
-        else str_new = str_new + str[i];
-    }  
-    return str_new;
+function strip (str)
+{   
+    return str_res = str.split(" ").filter(NotNull).join(" ");
 }
 
 function reverse(str)
