d62abf2478591c71b21f74ce2949463e3fee180e
Zakhar Pustovgarov <deadspace1999@mail.ru>
2019-12-24 20:00:21 +0400

    Написан второй тест

--- index.spec.js
+++ index.spec.js
@@ -6,4 +6,13 @@ describe('Puppeteer', () => {
   it('should be titled "puppeteer check"', async () => {
     await expect(page.title()).resolves.toMatch('puppeteer check');
   });
+
+  it('test of inputs', async () => {
+    await page.click('#a1');
+    await page.type('#a1','asdfr');
+    var openedInputText = await page.$eval('#a1', el => el.value);
+    expect(openedInputText).toEqual('asdfr');
+    var closedInputText = await page.$eval('#a2', el => el.value);
+    expect(closedInputText).toEqual('asdfr');
+  });
 });
